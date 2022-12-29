561f876ed6be08c2a3e2bae1d4bb716e49e23ea3
Gefeste <gefeste111@gmail.com>
2022-12-30 00:09:42 +0100

    hw 7 part 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-чі
+
+
+function createImageGallery(galleryItems) {
+
+    return galleryItems.map(({ preview, original, description }) => {
+        
+return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+}).join('')
+
+}
+
+const galleryElList = document.querySelector('.gallery');
+
+galleryElList.insertAdjacentHTML('afterbegin', createImageGallery(galleryItems));
+
+galleryElList.addEventListener('click', selectImage);
+
+function selectImage(img) {
+     img.preventDefault()
+
+    if (!img.target.classList.contains("gallery__image")) {
+        return
+    }
+    
+
+    const instance = basicLightbox.create(`
+    <img src="${img.target.dataset.source}" width="800" height="600">
+`)
+
+ instance.show()
+}
+
+
